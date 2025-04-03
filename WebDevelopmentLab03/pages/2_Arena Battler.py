42c2ae872a472f3e3e79fbd54b5204958ed44e04
gustavoscherrer <110138654+gustavoscherrer@users.noreply.github.com>
2025-04-03 16:46:18 -0400

    Delete WebDevelopmentLab03 directory

--- WebDevelopmentLab03/pages/2_Arena Battler.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-import os
-
-model = genai.GenerativeModel("gemini-1.5-flash") #this is the free model of google gemini
-response = model.generate_content("Write a poem about how learning web development is fun!") #enter your prompt here!
-print(response.text) #dont forget to print your response!
